f7f25cb98373e73da591c8812d917b40c5112cc8
Ryan Tankeng <92751701+MrRyanWise@users.noreply.github.com>
2022-04-18 17:12:01 +0200

    Add files via upload

---
+++ data/ChatRooms.ts
@@ -0,0 +1,145 @@
+export default [{
+  id: '1',
+  users: [{
+    id: 'u1',
+    name: 'Vadim',
+    imageUri: 'https://img-19.commentcamarche.net/WNCe54PoGxObY8PCXUxMGQ0Gwss=/480x270/smart/d8c10e7fd21a485c909a5b4c5d99e611/ccmcms-commentcamarche/20456790.jpg',
+   }, {
+    id: 'u2',
+    name: 'Lukas',
+    imageUri: 'https://img-19.commentcamarche.net/WNCe54PoGxObY8PCXUxMGQ0Gwss=/480x270/smart/d8c10e7fd21a485c909a5b4c5d99e611/ccmcms-commentcamarche/20456790.jpg',
+   }],
+  lastMessage: {
+    id: 'm1',
+    content: 'Well done this sprint, guys!',
+    createdAt: '2020-10-03T14:48:00.000Z',
+  }
+}, {
+  id: '2',
+  users: [{
+    id: 'u1',
+    name: 'Vadim',
+     imageUri: 'https://img-19.commentcamarche.net/WNCe54PoGxObY8PCXUxMGQ0Gwss=/480x270/smart/d8c10e7fd21a485c909a5b4c5d99e611/ccmcms-commentcamarche/20456790.jpg',
+  }, {
+    id: 'u3',
+    name: 'Daniil',
+    imageUri: 'https://img-19.commentcamarche.net/WNCe54PoGxObY8PCXUxMGQ0Gwss=/480x270/smart/d8c10e7fd21a485c909a5b4c5d99e611/ccmcms-commentcamarche/20456790.jpg',
+  }],
+  lastMessage: {
+    id: 'm2',
+    content: 'How are you doing?',
+    createdAt: '2020-10-02T15:40:00.000Z',
+  }
+}, {
+  id: '3',
+  users: [{
+    id: 'u1',
+    name: 'Vadim',
+     imageUri: 'https://img-19.commentcamarche.net/WNCe54PoGxObY8PCXUxMGQ0Gwss=/480x270/smart/d8c10e7fd21a485c909a5b4c5d99e611/ccmcms-commentcamarche/20456790.jpg',
+  }, {
+    id: 'u4',
+    name: 'Alex',
+    imageUri: 'https://img-19.commentcamarche.net/WNCe54PoGxObY8PCXUxMGQ0Gwss=/480x270/smart/d8c10e7fd21a485c909a5b4c5d99e611/ccmcms-commentcamarche/20456790.jpg',
+  }],
+  lastMessage: {
+    id: 'm3',
+    content: 'Hi, Vadim.',
+    createdAt: '2020-10-02T14:48:00.000Z',
+  }
+}, {
+  id: '4',
+  users: [{
+    id: 'u1',
+    name: 'Vadim',
+    imageUri: 'https://img-19.commentcamarche.net/WNCe54PoGxObY8PCXUxMGQ0Gwss=/480x270/smart/d8c10e7fd21a485c909a5b4c5d99e611/ccmcms-commentcamarche/20456790.jpg',
+   }, {
+    id: 'u5',
+    name: 'Vlad',
+    imageUri: 'https://img-19.commentcamarche.net/WNCe54PoGxObY8PCXUxMGQ0Gwss=/480x270/smart/d8c10e7fd21a485c909a5b4c5d99e611/ccmcms-commentcamarche/20456790.jpg',
+   }],
+  lastMessage: {
+    id: 'm4',
+    content: 'Can you review my last merge',
+    createdAt: '2020-09-29T14:48:00.000Z',
+  }
+}, {
+  id: '5',
+  users: [{
+    id: 'u1',
+    name: 'Vadim',
+     imageUri: 'https://img-19.commentcamarche.net/WNCe54PoGxObY8PCXUxMGQ0Gwss=/480x270/smart/d8c10e7fd21a485c909a5b4c5d99e611/ccmcms-commentcamarche/20456790.jpg',
+  }, {
+    id: 'u6',
+    name: 'Elon Musk',
+    imageUri: 'https://img-19.commentcamarche.net/WNCe54PoGxObY8PCXUxMGQ0Gwss=/480x270/smart/d8c10e7fd21a485c909a5b4c5d99e611/ccmcms-commentcamarche/20456790.jpg',
+    }],
+  lastMessage: {
+    id: 'm5',
+    content: 'I would be happy',
+    createdAt: '2020-09-30T14:48:00.000Z',
+  }
+}, {
+  id: '6',
+  users: [{
+    id: 'u1',
+    name: 'Vadim',
+     imageUri: 'https://img-19.commentcamarche.net/WNCe54PoGxObY8PCXUxMGQ0Gwss=/480x270/smart/d8c10e7fd21a485c909a5b4c5d99e611/ccmcms-commentcamarche/20456790.jpg',
+  }, {
+    id: 'u7',
+    name: 'Adrian',
+    imageUri: 'https://img-19.commentcamarche.net/WNCe54PoGxObY8PCXUxMGQ0Gwss=/480x270/smart/d8c10e7fd21a485c909a5b4c5d99e611/ccmcms-commentcamarche/20456790.jpg',
+    }],
+  lastMessage: {
+    id: 'm6',
+    content: 'I have a solution',
+    createdAt: '2020-10-02T15:40:00.000Z',
+  }
+}, {
+  id: '7',
+  users: [{
+    id: 'u1',
+    name: 'Vadim',
+     imageUri: 'https://img-19.commentcamarche.net/WNCe54PoGxObY8PCXUxMGQ0Gwss=/480x270/smart/d8c10e7fd21a485c909a5b4c5d99e611/ccmcms-commentcamarche/20456790.jpg',
+  }, {
+    id: 'u8',
+    name: 'Borja',
+    imageUri: 'https://img-19.commentcamarche.net/WNCe54PoGxObY8PCXUxMGQ0Gwss=/480x270/smart/d8c10e7fd21a485c909a5b4c5d99e611/ccmcms-commentcamarche/20456790.jpg',
+    }],
+  lastMessage: {
+    id: 'm7',
+    content: 'How are you doing?',
+    createdAt: '2020-10-02T15:40:00.000Z',
+  }
+}, {
+  id: '8',
+  users: [{
+    id: 'u1',
+    name: 'Vadim',
+     imageUri: 'https://img-19.commentcamarche.net/WNCe54PoGxObY8PCXUxMGQ0Gwss=/480x270/smart/d8c10e7fd21a485c909a5b4c5d99e611/ccmcms-commentcamarche/20456790.jpg',
+  }, {
+    id: 'u9',
+    name: 'Mom',
+    imageUri: 'https://image.shutterstock.com/image-vector/super-mom-hero-superhero-cartoon-600w-720015928.jpg',
+  }],
+  lastMessage: {
+    id: 'm8',
+    content: 'Dear, did you eat?',
+    createdAt: '2020-09-27T15:40:00.000Z',
+  }
+}, {
+  id: '9',
+  users: [{
+    id: 'u1',
+    name: 'Vadim',
+     imageUri: 'https://img-19.commentcamarche.net/WNCe54PoGxObY8PCXUxMGQ0Gwss=/480x270/smart/d8c10e7fd21a485c909a5b4c5d99e611/ccmcms-commentcamarche/20456790.jpg',
+  }, {
+    id: 'u10',
+    name: 'Angelina Jolie',
+    imageUri: 'https://lkbkspro.s3.amazonaws.com/atelier-management/gs_58d933b8-98b4-468e-b229-43100a9620a7.jpg',
+  }],
+  lastMessage: {
+    id: 'm9',
+    content: 'Meet me at the same place',
+    createdAt: '2020-09-25T15:40:00.000Z',
+  },
+}]
